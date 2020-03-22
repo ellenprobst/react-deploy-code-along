95eada925e10a620dfbe16fce87abbbf2a9f3a13
Ellen Probst <eprobst@twg.ca>
2020-03-22 12:40:09 -0400

    here is a change

--- api/tokens/tokenService.js
+++ api/tokens/tokenService.js
@@ -1,21 +1,20 @@
-const jwt = require('jsonwebtoken');
-const KEY = 'secret phrase';
+const jwt = require('jsonwebtoken')
+const KEY = 'secret phrase'
 
-exports.createToken = (user) => {
-  const token = jwt.sign(user, KEY);
-  return token;
+exports.createToken = user => {
+  const token = jwt.sign(user, KEY)
+  return token
 }
 
-exports.verifyToken = async (token) => {
-  let user;
+exports.verifyToken = async token => {
+  let user
   jwt.verify(token, KEY, (err, decoded) => {
-    console.log(err);
-    if (err)  {
-      throw err;
+    if (err) {
+      throw err
     }
 
-    user = decoded;
-  });
+    user = decoded
+  })
 
-  return user;
+  return user
 }
