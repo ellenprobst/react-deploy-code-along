08f5c0a279ce89a8865eb1be8f8fe638f564e28b
Ellen Probst <eprobst@twg.ca>
2020-03-22 12:20:48 -0400

    makes changes for deploy

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
 
-const userRouter = require('./api/routes/users/userRoutes');
-const notesRouter = require('./api/routes/notes/notesRoutes');
+const userRouter = require('./api/routes/users/userRoutes')
+const notesRouter = require('./api/routes/notes/notesRoutes')
 
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/api/users', userRouter);
-app.use('/api/notes', notesRouter);
+const PORT = process.env.PORT || 8080
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/note-app'
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use('/api/users', userRouter)
+app.use('/api/notes', notesRouter)
 
 // for ALL routes that don't match, serve our react bundle.
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'build')))
 
+// allows rreact router to take care of routing
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 mongoose
-  .connect('mongodb://localhost:27017/note-app',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  )
-  .then(() => {
-    app.listen('8080', () => {
-      console.log('server is running on port 8080');
-    });
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
-  .catch((err) => console.log(err));
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('server is running on port 8080')
+    })
+  })
+  .catch(err => console.log(err))
